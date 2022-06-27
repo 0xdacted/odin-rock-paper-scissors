1f40b82117ce6b6c758dae951c4a0b8459574f8c
Red <crabmarket56@gmail.com>
2022-06-26 21:21:20 -0400

    Add player play function

--- external-js.js
+++ external-js.js
@@ -6,18 +6,36 @@ function computerPlay() {
    let computerSelection = getRndInteger(1,3); 
    switch(computerSelection) {
     case 1:
-        return "The computer chose rock!"
+        return "rock"
         break;
     case 2:
-        return "The computer chose paper!"
+        return "paper"
         break;
     case 3:
-        return "The computer chose Scissors!"
+        return "scissors"
         break;
-
-
    }
-
-   
-   
 }
+
+function playerPlay() {
+    let playerSelection = prompt("Please type rock, paper, or scissors").toLowerCase();
+    switch(playerSelection) {
+        case playerSelection.textContent = "rock":
+            return "rock"
+            break;
+        
+        case playerSelection.textContent = "paper":
+            return "paper"
+            break;
+        
+        case playerSelection.textContent = "scissors":
+            return "scissors"
+            break;
+
+        default:
+            return "Please type rock, paper, or scissors"
+    }
+  
+
+}
+   
